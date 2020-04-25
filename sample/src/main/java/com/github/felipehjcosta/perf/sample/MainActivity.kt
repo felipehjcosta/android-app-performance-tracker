a6a41a214c867961744f403bb86822f5c8bc7a16
felipehjcosta <felipe.h.j.costa@gmail.com>
2020-04-25 11:02:06 -0300

    Migrate sample to AndroidX

--- sample/src/main/java/com/github/felipehjcosta/perf/sample/MainActivity.kt
+++ sample/src/main/java/com/github/felipehjcosta/perf/sample/MainActivity.kt
@@ -1,12 +1,5 @@
 package com.github.felipehjcosta.perf.sample
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
