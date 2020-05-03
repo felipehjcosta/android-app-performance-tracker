14e3b21a1684b5bb418b1967408d84c8f1ec3afb
felipehjcosta <felipe.h.j.costa@gmail.com>
2020-05-02 23:11:30 -0300

    Add Firebase delivery DSL to sample

--- sample/src/main/java/com/github/felipehjcosta/perf/sample/MainApplication.kt
+++ sample/src/main/java/com/github/felipehjcosta/perf/sample/MainApplication.kt
@@ -3,12 +3,17 @@ package com.github.felipehjcosta.perf.sample
 import android.app.Application
 import com.github.felipehjcosta.perf.PerformanceTracker
 import com.github.felipehjcosta.perf.console.logging
+import com.github.felipehjcosta.perf.firebase.firebaseDelivery
 import com.github.felipehjcosta.perf.with
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        PerformanceTracker.initialize() with logging(BuildConfig.DEBUG)
+        PerformanceTracker.initialize() with {
+            logging(true)
+            firebaseDelivery(FirebaseAnalytics.getInstance(this@MainApplication))
+        }
     }
 }
